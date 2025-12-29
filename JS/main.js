3819848b1340d5e09432f5680866ab8fcfd9aa5a
arunna-sudo <arun.na@ku.th>
2025-12-29 16:52:52 +0700

    js file

---
+++ JS/main.js
@@ -0,0 +1,40 @@
+/*
+&& เเละ
+|| หรือ
+! not หรือไม่
+*/
+/*
+let number1 = 5
+let number2 = 8
+// ture && ture 
+let condition = !(number1 >= 3 || number2 >= 10)
+console.log("condition :",condition);
+
+let age = 25;
+let gender = "ชาย";
+
+if (age >= 20 && gender == "ชาย"){
+    console.log("ผู้ชายอายุ 20ปีขึ้นไป");
+}
+*/
+/*
+let number = 20 
+
+if (number % 2 == 0) {
+    console.log ("เป็นเลขคู่")
+}
+ */
+/*
+loop statments:
+while 
+for
+*/
+let counter = -5;
+while(counter< 10){
+    console.log('hello world');
+    counter = counter + 1
+}
+
+for(let i = 0; i < 10; i++){
+    console.log('hello world form loop');
+}
